ae3d1a254d267d7fdb424ac4f1a1a10beb49357a
heruiksan <heruiksan43@gmail.com>
2023-04-05 17:05:22 +0700

    Menambahkan class datamahasiswa_1302210009

--- modul7_kelompok_03/Program.cs
+++ modul7_kelompok_03/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using modul7_kelompok_03;
+
+DataMahasiswa__1302210009 d1 = new DataMahasiswa__1302210009();
+d1.ReadJSON();
